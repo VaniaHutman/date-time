3336b3185ae6d5de60258bb359618dbe370110df
VaniaHutman <v6160@mail.ru>
2020-08-04 12:02:14 +0300

    initial commit

---
+++ date-time/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace date_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = new DateTime(2020, 8, 1);
+            DateTime end = new DateTime(2020, 8, 31);
+            DateTime[] holidays = { new DateTime(2020, 8, 24) };
+            int work_days = CountWorkingDays(start, end, holidays);
+            Console.WriteLine(work_days);
+        }
+
+        public static int CountWorkingDays(DateTime start, DateTime end, DateTime[] holidays)
+        {
+            int counter = 0;
+            bool searched;
+
+            while (start != end)
+            {
+                if (start.DayOfWeek != DayOfWeek.Saturday && start.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    searched = false;
+                    for (int i = 0; i < holidays.Length; i++)
+                    {
+                        if (start == holidays[i])
+                        {
+                            searched = true;
+                        }
+                    }
+                    if (searched != true)
+                    {
+                        counter++;
+                    }
+                }
+                start = start.AddDays(1);
+            }
+            return counter;
+        }
+    }
+}
